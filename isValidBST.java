8f9e6d1bf21f535f213fd8936d9aa1149f44858a
kyleyiyang <kyleyiyang@gmail.com>
2021-03-31 21:35:22 -0400

    Update isValidBST.java

--- isValidBST.java
+++ isValidBST.java
@@ -24,3 +24,24 @@ class Solution {
         return recursion(root.left,minVal,root.val) && recursion(root.right,root.val,maxVal);
     }
 }
+
+*********************************************************
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if (root==null) return true;
+        Stack<TreeNode> stack=new Stack<>();
+        TreeNode pre=null;
+        while (root!=null || !stack.isEmpty()) {
+            while(root!=null) {
+                stack.push(root);
+                root=root.left;
+            }
+            root=stack.pop();
+            if (pre!=null && root.val<=pre.val) return false;
+            pre=root;
+            root=root.right;
+        }
+        return true;
+    }
+}
